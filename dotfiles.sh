dafed13d64fafe5e1f6f62b1621706163815db37
William Melody <hi@williammelody.com>
2014-12-15 11:53:29 -0800

    Ascii dotfiles for dotfiles.sh

--- dotfiles.sh
+++ dotfiles.sh
@@ -317,7 +317,13 @@ EOM
 help() {
   if [[ $# = 0 ]]; then
     cat <<EOM
-dotfiles
+
+      _       _    __ _ _
+     | |     | |  / _(_) |
+   __| | ___ | |_| |_ _| | ___  ___
+  / _\` |/ _ \\| __|  _| | |/ _ \\/ __|
+ | (_| | (_) | |_| | | | |  __/\\__ \\
+  \\__,_|\\___/ \\__|_| |_|_|\\___||___/
 
 Tasks for managing OS X dotfiles and environment config.
 
