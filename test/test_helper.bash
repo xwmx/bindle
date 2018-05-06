9d04cdae651dde477c80b86eafc289dea660305f
William Melody <hi@williammelody.com>
2018-05-06 15:32:56 -0700

    Rename `$_BINDLE_TMP_DIR` to `$_BINDLE_TMP_PATH`.

--- test/test_helper.bash
+++ test/test_helper.bash
@@ -12,23 +12,23 @@ setup() {
   # The location of the `bindle` script being tested.
   export _BINDLE="${BATS_TEST_DIRNAME}/../bindle"
 
-  export _BINDLE_TMP_DIR
-  _BINDLE_TMP_DIR="$(mktemp -d /tmp/bindle_test.XXXXXX)" || exit 1
+  export _BINDLE_TMP_PATH
+  _BINDLE_TMP_PATH="$(mktemp -d /tmp/bindle_test.XXXXXX)" || exit 1
 
-  echo "1" > "${_BINDLE_TMP_DIR}/.one"
-  echo "2" > "${_BINDLE_TMP_DIR}/.two"
-  echo "3" > "${_BINDLE_TMP_DIR}/.three"
+  echo "1" > "${_BINDLE_TMP_PATH}/.one"
+  echo "2" > "${_BINDLE_TMP_PATH}/.two"
+  echo "3" > "${_BINDLE_TMP_PATH}/.three"
 
-  export _HOME_PATH="${_BINDLE_TMP_DIR}"
+  export _HOME_PATH="${_BINDLE_TMP_PATH}"
   export _BINDLEPATH="${_HOME_PATH}/.bindle"
 }
 
 teardown() {
-  if [[ -n "${_BINDLE_TMP_DIR}" ]] &&
-     [[ -e "${_BINDLE_TMP_DIR}" ]] &&
-     [[ "${_BINDLE_TMP_DIR}" =~ ^/tmp/bindle_test ]]
+  if [[ -n "${_BINDLE_TMP_PATH}" ]] &&
+     [[ -e "${_BINDLE_TMP_PATH}" ]] &&
+     [[ "${_BINDLE_TMP_PATH}" =~ ^/tmp/bindle_test ]]
   then
-    rm -r "${_BINDLE_TMP_DIR}"
+    rm -r "${_BINDLE_TMP_PATH}"
   fi
 }
 
